11eb767a34f7853c3b142a598a0a8f9fef325f46
kingharianja <rmharianja@gmail.com>
2020-03-02 16:09:50 +0700

    add some files

---
+++ inc/sendEmail.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+$name = $_POST['contactName'];
+$email = $_POST['contactEmail'];
+$subject = $_POST['contactSubject'];
+$message = $_POST['contactMessage'];
+
+    function send_mail($email,$message,$subject)
+    { 
+    require_once('mailer/class.phpmailer.php');
+    $mail = new PHPMailer();
+    $mail->IsSMTP(); 
+    $mail->SMTPDebug  = 0;                     
+    $mail->SMTPAuth   = true;                  
+    $mail->SMTPSecure = "ssl";                 
+    $mail->Host       = "smtp.gmail.com";      
+    $mail->Port       = 465;             
+    $mail->AddAddress($email);
+    $mail->Username="codepuisi@gmail.com";  
+    $mail->Password="erzascarlet";            
+    $mail->SetFrom('codepuisi@gmail.com','Code Puisi');
+    $mail->AddReplyTo("codepuisi@gmail.com","Code Puisi");
+    $mail->Subject    = $subject;
+    $mail->MsgHTML($message);
+    $mail->Send();
+    }
+    $message = "Nama: $name <br> Email : $email <br> Subject : $subject <br> Message : $message";
+    send_mail('rmharianja@gmail.com',$message,$subject); 
+    echo "OK";
